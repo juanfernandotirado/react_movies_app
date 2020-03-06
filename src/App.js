cbcd3285f9391e64ab621fc93cbf92cb8e3ccf44
Juan_Tirado <50636966+juanfernandotirado@users.noreply.github.com>
2020-03-06 10:19:57 -0800

    Tab Bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MoviesContainer from './components/containers/MoviesContainer'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MoviesContainer from "./components/containers/MoviesContainer";
+
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 function App() {
+  const [value, setValue] = React.useState(0);
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className="App">
-
-    <MoviesContainer />
-
+      <h1>React Movies App</h1>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Movies" />
+          <Tab label="Search" />
+          <Tab label="TV" />
+        </Tabs>
+      </AppBar>
+      <MoviesContainer />
     </div>
   );
 }
